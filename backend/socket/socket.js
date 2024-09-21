c2b272655835a6e0392e912df10fc2e144f5c24e
Tushar Sukhananadi <tusharsukhanandi@yahoo.com>
2024-09-21 10:32:56 +0530

    cors

--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -9,7 +9,7 @@ console.log(process.env.CORS_ORIGIN_URL);
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "https://localhost:3000",
+    origin: "*",
     methods: ["GET", "POST"],
   },
 });
