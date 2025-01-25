fbe92b893ad5b08dc5b53d55730ec2a46babcb2f
Tushar Sukhananadi <tusharsukhanandi@yahoo.com>
2025-01-25 11:56:24 +0530

    trying to resolve message delay 2

--- frontend/src/redux/slices/messagesSlice.js
+++ frontend/src/redux/slices/messagesSlice.js
@@ -8,7 +8,7 @@
                 return action.payload
             },
             addMessage: (state, action) => {
-                state[state.length - 1] = action.payload
+                state[state.length] = action.payload
             }
         }
     })
